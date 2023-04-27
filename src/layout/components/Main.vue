aea6ff1cb3a7fca339611162b7397d5fb50b17eb
Tromp <17688881701@163.com>
2023-04-27 20:12:53 +0800

    初始化

--- src/layout/components/Main.vue
+++ src/layout/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <el-scrollbar style="width: 100%;" view-style="min-height: 100%; display: flex;">
-    <el-main style="height: 100%;">
+    <el-main>
       <router-view></router-view>
     </el-main>
   </el-scrollbar>
