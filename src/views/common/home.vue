83d8349d3dac7a09ddf802f112710fa25939ab0a
liuyun <17688881701@163.com>
2023-05-09 11:08:27 +0800

    初始化

--- src/views/common/home.vue
+++ src/views/common/home.vue
@@ -1,72 +1,105 @@
 <template>
-  <div id="container" style="height: 100%;"/>
+  <div ref="container" style="width: 100%;height: 100%;"/>
 </template>
+<script>
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import a from '@/assets/img/earth_texture.jpg'
 
-<script lang="tsx" setup>
-import {
-  Scene,
-  PerspectiveCamera,
-  BoxGeometry,
-  MeshBasicMaterial,
-  Mesh,
-  WebGLRenderer,
-} from "three";
-// 导入轨道控制器
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-onMounted(() => {
+export default defineComponent({
+  setup() {
+    const container = ref(null)
+    
+    let scene, camera, renderer, earth
 
-  // 1. 创建场景
-  const scene = new Scene();
-  // 2. 创建相机
-  /**
-   * 三种相机
-   * 透视相机： PerspectiveCamera
-   * 正交相机： OrthographicCamera
-   * 立方体相机： CubeCamera
-   */
-  const camera = new PerspectiveCamera(
-    75, // 视场角，表示相机的可视角度
-    window.innerWidth / window.innerHeight, // 屏幕宽高比，表示渲染的画面的宽高比
-    1, // 近平面距离，表示相机能够看到的最短距离
-    1000 // 远平面距离，表示相机能够看到的最远距离
-  );
-  // 设置相机位置
-  camera.position.set(0, 0, 5);
-  scene.add(camera);
-  // 创建一个几何体
-  /**
-   * 立方体几何体（BoxGeometry）
-   * 球体几何体（SphereGeometry）
-   * 圆柱体几何体（CylinderGeometry）
-   * 平面几何体（PlaneGeometry）
-   */
-  const cubeGeometry = new BoxGeometry(1, 1, 1);
-  // 给几何体加材质
-  const cubeMaterial = new MeshBasicMaterial({ color: 0xffff00 });
-  const cube = new Mesh(cubeGeometry, cubeMaterial);
-  scene.add(cube);
-  // 初始化渲染器
-  /**
-   * 三种渲染器
-   * WebGL 渲染器（WebGLRenderer）
-   * SVG 渲染器（SVGRenderer）
-   * CSS2D 渲染器（CSS2DRenderer）
-   */
-  const renderer = new WebGLRenderer();
-  // 设置渲染器的尺寸
-  renderer.setSize(document.getElementById('container')?.offsetWidth, document.getElementById('container')?.offsetHeight);
-  
-  // 将webgl渲染的东西放到body上
- (document.getElementById('container') as HTMLElement).appendChild(renderer.domElement);
+    onMounted(() => {
+    	// 初始化场景
+      initScene()
+      // 初始化相机
+      initCamera()
+      // 初始化光线
+      initLight()
+      // 初始化渲染器
+      initRenderer()
+      // 初始化地球
+      initEarth()
+      // 添加轨道控制
+      initOrbitControls()
+      // 初始化动画
+      initAnimation()
+      
+    })
 
-  // 创建轨道控制器
-  const controls = new OrbitControls(camera, renderer.domElement);
-  function render() {
-    // 使用渲染通过向相机将场景渲染出来
-    renderer.render(scene, camera);
-    // 渲染下一帧就会调用这个函数
-    requestAnimationFrame(render);
+    onUnmounted(() => {
+      // 销毁
+      cancelAnimationFrame(animationId)
+    })
+
+    const initScene = () => {
+      scene = new THREE.Scene()
+    }
+
+    const initCamera = () => {
+      const width = container.value.offsetWidth
+      const height = container.value.offsetHeight
+      const fov = 25
+      const aspect = width / height
+      const near = 1
+      const far = 1000
+      // 透视相机
+      camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+      camera.position.z = 500
+    }
+
+    const initLight = () => {
+      // 点光源，白色，强度1，距离100
+      const pointLight = new THREE.PointLight(0xffffff, 1, 100)
+      pointLight.position.set(0, 0, 300)
+      scene.add(pointLight)
+      // 环境光，银灰色
+      const ambient = new THREE.AmbientLight(0xc0c0c0)
+      scene.add(ambient)
+    }
+
+    const initRenderer = () => {
+      renderer = new THREE.WebGLRenderer({ antialias: true })
+      const width = container.value.offsetWidth
+      const height = container.value.offsetHeight
+      renderer.setSize(width, height)
+      // 添加渲染器到DOM中
+      container.value.appendChild(renderer.domElement)
+    }
+
+    const initOrbitControls = () => {
+      const controls = new OrbitControls(camera, renderer.domElement); 
+    }
+
+    const initEarth = () => {
+      const earthRadius = 100
+      const earthGeometry = new THREE.SphereGeometry(earthRadius, 50, 50)
+      // 纹理贴图材质
+      const earthMaterial = new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load(a) })
+      earth = new THREE.Mesh(earthGeometry, earthMaterial)
+      // 在场景中添加地球网格
+      scene.add(earth)
+    }
+
+    let animationId = null
+    
+    const initAnimation = () => {
+      const animate = () => {
+        // 使地球旋转
+        earth.rotation.y += 0.005
+        // 渲染场景和相机
+        renderer.render(scene, camera)
+        animationId = requestAnimationFrame(animate)
+      }
+      animate()
+    }
+
+    return {
+      container
+    }
   }
-  render();
 })
 </script>
