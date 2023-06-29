fdd7c0a7ce599070468fcbedca080c1bb03f675a
liuyun <17688881701@163.com>
2023-06-29 19:49:14 +0800

    初始化

--- src/views/common/home.vue
+++ src/views/common/home.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>home</div>
+  <div style="width: 100%; height: 99%">
+    <vc-config-provider :cesium-path="vcConfig.cesiumPath">
+      <vc-viewer :show-credit="false" />
+    </vc-config-provider>
+  </div>
 </template>
-<script>
-export default defineComponent({
-  name: 'Home',
-  setup() {},
+
+<script setup>
+import { VcConfigProvider, VcViewer } from 'vue-cesium';
+
+const vcConfig = reactive({
+  cesiumPath: 'https://unpkg.com/cesium@latest/Build/Cesium/Cesium.js',
 });
 </script>
