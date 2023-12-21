3091600f2eee985b7a30ae4a3da8f9bd953e5d8d
liuyun <liuyun1@infore.com>
2023-12-21 19:19:21 +0800

    初始化

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -17,6 +17,7 @@ t;
 declare module '@/api/*.js';
 declare module '@/store/*.ts';
 declare module '@/hook/*.ts';
+declare module '@/store/*.js';
 declare module '@/utils/*.js';
 declare module '../mock/*.ts';
 declare module 'element-plus/dist/locale/zh-cn.mjs';
